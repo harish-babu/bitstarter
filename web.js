f9511fe05208ccd1df2acf99f94b65892d68a10c
Harish Babu <harish@ubuntu.(none)>
2013-07-17 20:21:13 +0530

    changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require ('fs');
+  var filename = "index.html"l
+  var buffer = fs.readFileSync (filename);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
